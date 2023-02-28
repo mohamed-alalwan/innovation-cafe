de8c141bdedfa82d282e313c0a65e5d1799eb420
mohamedalalwan <mohamedalalwan98@gmail.com>
2023-02-28 18:55:13 +0300

    added dynamic nav bar for items

--- routes/item.js
+++ routes/item.js
@@ -4,18 +4,18 @@ const router = express.Router()
 const itemCntrl = require('../controllers/item');
 
 //multer initialization
-const multer = require('multer');
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, 'public/images/items');
-    },
-    filename: (req, file, cb) => {
-        console.log(file);
-        cb(null, Date.now() + '-' + file.originalname);
-    }
-});
-const upload = multer({storage: storage});
-router.use(upload.single('imageURL'));
+// const multer = require('multer');
+// const storage = multer.diskStorage({
+//     destination: (req, file, cb) => {
+//         cb(null, 'public/images/items');
+//     },
+//     filename: (req, file, cb) => {
+//         console.log(file);
+//         cb(null, Date.now() + '-' + file.originalname);
+//     }
+// });
+// const upload = multer({storage: storage});
+// router.use(upload.single('imageURL'));
 
 // Call API
 router.get('/index', itemCntrl.item_index_get)
@@ -25,5 +25,8 @@ router.get('/show', itemCntrl.item_show_get)
 router.get('/delete', itemCntrl.item_delete_get)
 router.get('/edit', itemCntrl.item_edit_get)
 router.post('/edit', itemCntrl.item_edit_post)
+
+//test firebase storage upload again
+
 
 module.exports = router
