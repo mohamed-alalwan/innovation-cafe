7db7c1b797c3874fa536e674b3c47e04433cf104
msigp60 <ahmedalslaibi@gmail.com>
2023-02-28 10:59:43 +0300

    add menu HTML Ahmed+Mohamed AbdulRahim same laptop

--- controllers/item.js
+++ controllers/item.js
@@ -1,33 +1,37 @@
 // Model
-const Item = require ('../models/Item')
-const firebase = require('firebase/auth');
+const Item = require("../models/Item");
+const firebase = require("firebase/auth");
 const auth = firebase.getAuth();
 
 // HTTP index get items
 exports.item_index_get = (req, res) => {
-    res.render('item/index', {
-        auth: auth.currentUser
-    })
+    Item.find()
+        .then(items => {
+            res.render('item/index', {items, auth: auth.currentUser})
+        })
+        .catch(err => {
+            console.log(err)
+        })
 }
 
-// HTTP create Items get 
+// HTTP create Items get
 exports.item_create_get = (req, res) => {
-    Item.find()
-    .then((items) => {
-        res.render('item/add', {items, auth: auth.currentUser})
-    })
-}
+  Item.find().then((items) => {
+    res.render("item/add", { items, auth: auth.currentUser });
+  });
+};
 
 // HTTP Post Items Post
 exports.item_create_post = (req, res) => {
-    let item = new Item(req.body)
+  let item = new Item(req.body);
 
-    item.save()
+  item
+    .save()
     .then(() => {
-        res.redirect('/item/index')
+      res.redirect("/item/index");
     })
     .catch((err) => {
-        console.log(err);
-        res.send('Error please try again later')
-    })
-}
+      console.log(err);
+      res.send("Error please try again later");
+    });
+};
