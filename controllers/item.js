2a3e0c48f1715ed4b77287ff377afda6191b4ef7
mohamedalalwan <mohamedalalwan98@gmail.com>
2023-02-28 20:15:44 +0300

    added search function to items, fixed image url, now the images can be deleted, edited, removed with the items accordingly.

--- controllers/item.js
+++ controllers/item.js
@@ -3,23 +3,30 @@ const Item = require("../models/Item");
 const firebase = require("firebase/auth");
 const auth = firebase.getAuth();
 const authCntrl = require("../controllers/auth");
+const fs = require("fs");
 
 // HTTP index get items
 exports.item_index_get = (req, res) => {
-    Item.find()
-    .then( async (items) => {
-        const user = await authCntrl.returnCurrentUser();
-        res.render('item/index', 
-            {
-            items, 
-            auth: auth.currentUser,
-            user
-            }
-        )
-    })
-    .catch(err => {
-        console.log(err)
-    })
+    //search for items if search term is provided
+    if (req.query.search) {
+        Item.find({title: new RegExp(req.query.search, "i")})
+      .then(async (items) => {
+            const user = await authCntrl.returnCurrentUser();
+            res.render('item/index', {items, auth: auth.currentUser, user})
+        })
+      .catch(err => {
+            console.log(err)
+        })
+    } else {
+        Item.find()
+      .then(async (items) => {
+            const user = await authCntrl.returnCurrentUser();
+            res.render('item/index', {items, auth: auth.currentUser, user})
+        })
+      .catch(err => {
+            console.log(err)
+        })
+    }
 }
 
 // HTTP create Items get
@@ -32,7 +39,8 @@ exports.item_create_get = (req, res) => {
 // HTTP Post Items Post
 exports.item_create_post = async (req, res) => {
     let item = new Item(req.body);
-    item.imageURL = req.file.path;
+    const imageURL = req.file.path.replace("public", "");
+    item.imageURL = imageURL;
     item
     .save()
     .then(() => {
@@ -50,9 +58,7 @@ exports.item_show_get = (req, res) => {
     Item.findById(req.query.id)
     .then(async (item) => {
         const user = await authCntrl.returnCurrentUser();
-        console.log(item);
         res.render('item/show', {item, auth: auth.currentUser, user}) 
-        
     })
     .catch(err => {
         console.log(err);
@@ -62,7 +68,12 @@ exports.item_show_get = (req, res) => {
 // HTTP Delete item
 exports.item_delete_get = (req, res) => {
     Item.findByIdAndDelete(req.query.id)
-    .then(() => {
+    .then((item) => {
+        fs.unlink('public'+item.imageURL, (err) => {
+            if (err) {
+                console.log(err);
+            }
+        });
         res.redirect('/item/index')
     })
     .catch(err => {
@@ -90,10 +101,23 @@ exports.item_edit_get = (req, res) => {
 // HTTP Edit - Post
 exports.item_edit_post = (req, res) => {
     const {id, title, des, category, price} = req.body
-    Item.findByIdAndUpdate(id, {title, des, category, price}, {new:true})
-    .then(updatedItem => {
-        res.redirect('/item/index')
+    const imageURL = req.file.path.replace("public", "");
+    Item.findById(id)
+    .then(async (item) => {
+        fs.unlink('public'+item.imageURL, (err) => {
+            if (err) {
+                console.log(err);
+            }else{
+                Item.findByIdAndUpdate(id, {imageURL, title, des, category, price}, {new:true})
+                .then(updatedItem => {
+                    res.redirect('/item/index');
+                });
+            }
+        }); 
     })
+    .catch(err => {
+        console.log(err);
+    });
 }
 
 
