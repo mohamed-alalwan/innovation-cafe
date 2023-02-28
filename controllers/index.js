354b2f62fe44824d2f4bb38391fb29d61686a0b9
mohamedalalwan <mohamedalalwan98@gmail.com>
2023-02-28 11:13:42 +0300

    added user model variable in home page

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,12 @@
 const firebase = require('firebase/auth');
 const auth = firebase.getAuth();
+const authCntrl = require('../controllers/auth');
 
 //method to handle home page at root route
-exports.home_index_get = function(req, res) {
+exports.home_index_get = async function(req, res) {
+    const user = await authCntrl.returnCurrentUser();
     res.render('home/index', {
-        auth: auth.currentUser
+            auth: auth.currentUser,
+            user
     });
 }
