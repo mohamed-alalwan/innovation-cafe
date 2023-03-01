d3cd338bd3f9dab14a423c1612cb2a3845cf96a1
mohamedalalwan <mohamedalalwan98@gmail.com>
2023-03-01 18:30:54 +0300

    cart stuff

--- controllers/cart.js
+++ controllers/cart.js
@@ -21,10 +21,10 @@ exports.cart_create_get = async (req, res) => {
     if(auth.currentUser){
         const itemID = req.query.id;
         User.findOne({ firebaseID: auth.currentUser.uid })
-        .then(user => {
+        .then(async (user) => {
             const cart = user.cart;
-            cart.push(itemID);
             //TODO : ADD LOOP TO CHECK IF ITEM IS ALREADY IN CART
+            cart.push(itemID);
             User.findByIdAndUpdate(user._id, {cart}, {new: true})
             .then(user => {
                 console.log(user);
@@ -32,6 +32,34 @@ exports.cart_create_get = async (req, res) => {
                 res.redirect('/cart/index');
             })
             .catch(err => {
+                console.log(err);
+            })
+
+        }).catch(err => {
+            console.log(err);
+        })
+    }else{
+        res.redirect("/item/index");
+        console.log('not authorized');
+    }
+}
+
+//remove from cart
+exports.cart_delete_get = async (req, res) => {
+    if(auth.currentUser){
+        const itemID = req.query.id;
+        User.findOne({ firebaseID: auth.currentUser.uid })
+       .then(async (user) => {
+            const cart = user.cart;
+            //TODO : ADD LOOP TO CHECK IF ITEM IS ALREADY IN CART
+            cart.splice(cart.indexOf(itemID), 1);
+            User.findByIdAndUpdate(user._id, {cart}, {new: true})
+           .then(user => {
+                console.log(user);
+                //success
+                res.redirect('/cart/index');
+            })
+           .catch(err => {
                 console.log(err);
             })
 
