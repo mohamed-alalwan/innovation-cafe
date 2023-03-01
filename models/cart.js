066de2ee7ca8d2b5f8b700a6ad4a151651a971d0
msigp60 <ahmedalslaibi@gmail.com>
2023-03-01 10:17:55 +0300

    added cart model + index view + empty route.js

---
+++ models/cart.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const cartSchema = mongoose.Schema({
+    totalPrice: {type:String},
+    area: String,
+    block: String,
+    street: String,
+    house: String,
+    phoneNumber: String,
+    items: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Item'
+    }]
+},
+{
+    timestamps: true //means createdAt and updatedAt.
+})
+
+//Model.
+const Cart = mongoose.model("Cart", cartSchema);
+
+//Export.
+module.exports = Cart;
