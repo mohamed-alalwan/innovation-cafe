64e050ca8d11a19ea306c300a2686d33d03b1c51
mohamedalalwan <mohamedalalwan98@gmail.com>
2023-03-02 19:02:02 +0300

    Add readme file to main

--- models/User.js
+++ models/User.js
@@ -1,4 +1,4 @@
-const mongoose = require ('mongoose')
+const mongoose = require ('mongoose');
 
 const userSchema = mongoose.Schema({
     firstName: {type:String, required: true},
@@ -19,8 +19,8 @@ const userSchema = mongoose.Schema({
 },
 {
     timestamps: true
-})
+});
 
-const User = mongoose.model('User', userSchema)
+const User = mongoose.model('User', userSchema);
 
-module.exports = User
+module.exports = User;
