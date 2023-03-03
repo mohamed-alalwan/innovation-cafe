55c692a371b90ab911a3550e46e977ec3bcfa422
mohamedalalwan <mohamedalalwan98@gmail.com>
2023-03-03 23:48:07 +0300

    modified format for getting env

--- generate-google-api-credentials.js
+++ generate-google-api-credentials.js
@@ -6,7 +6,8 @@ const credentials = {
   type: 'service_account',
   project_id: 'innovation-cafe-b0fe2',
   private_key_id: process.env.GOOGLE_PRIVATE_KEY_ID,
-  private_key: process.env.GOOGLE_PRIVATE_KEY,
+  private_key: process.env.GOOGLE_PRIVATE_KEY ? process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/gm, "\n")
+    : undefined,
   client_email: process.env.GOOGLE_CLIENT_EMAIL,
   client_id: process.env.GOOGLE_CLIENT_ID,
   auth_uri: 'https://accounts.google.com/o/oauth2/auth',
